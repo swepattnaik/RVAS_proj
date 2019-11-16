321a9a9c2db1528ee8138a79b89bdd8bcd10f745
shu2010 <s.pattnaik@garvan.org.au>
2019-11-16 15:24:43 +0000

    added pvalgt01 genes to total list of random genes

--- bootstrapping_strategy.R
+++ bootstrapping_strategy.R
@@ -21,17 +21,20 @@ Exome_pc123_srt_SKAT_case_enr_nCH_pvalgt01 <- lapply(Exome_pc123_srt_SKAT_case_e
 Exome_pc123_srt_SKAT_case_neg_enr_nCH_pval <- lapply(Exome_pc123_srt_SKAT_case_enr_nCH_pval, function(x)
   x[x$wt_diff < 0,])
 ##remove from gene_tot for bootstrapping
-gene_tot <- as.character(Exome_pc123_srt_SKAT_case_neg_enr_nCH_pval[[4]]$gene)
+gene_tot <- c(as.character(Exome_pc123_srt_SKAT_case_neg_enr_nCH_pval[[4]]$gene), 
+              as.character(Exome_pc123_srt_SKAT_case_enr_nCH_pvalgt01[[4]]$gene))
 gene_tot_rand <- gene_tot[!(gene_tot %in% as.character(Exome_pc123_srt_SKAT_case_enr_nCH_pval01[[4]]$gene))]
 ##build random samples for bootstrapping w/replacement
 #rand_samp <- sapply(gene_tot, function(x) {sample(1:1000, size = 905, prob = 1/905)})
 gene_rand <- list()
 for(i in 1:100){
-  gene_rand_ind <- sample(length(gene_tot_rand), size = 85, replace = T) ##size based on clique filtering
+#  gene_rand_ind <- sample(length(gene_tot_rand), size = 85, replace = T) ##size based on clique filtering
+  gene_rand_ind <- sample(length(gene_tot_rand), size = 100, replace = T)
   gene_rand[[i]] <- gene_tot[gene_rand_ind]
 }
 
 ##function for report degree of specific vertex of a subgraph
+can_net <- read.delim("~/VDLab_scripts/BioGrid/biogrid_db_all_subnet.sif", header = T, sep = " ", stringsAsFactor = F)
 get_deg_rand <- function(gene_list, gene_name){
   can_net_graph <- igraph::graph.data.frame(can_net, directed = F)
   can_net_graph1 <- igraph::simplify(can_net_graph, remove.loops=T, remove.multiple = T)
@@ -56,6 +59,40 @@ Exome_pc123_srt_SKAT_case_enr_nCH_ranked <- lapply(Exome_pc123_srt_SKAT_case_enr
 Exome_pc123_srt_SKAT_case_enr_nCH_ranked <- lapply(Exome_pc123_srt_SKAT_case_enr_nCH_ranked, function(x)
   x[order(x[,17], decreasing = T),])
 gender_PC123_cont_df <- Exome_pc123_srt_SKAT_case_enr_nCH_ranked[[4]]
+
+##function for degree detection
+library(igraph)
+library(ggnet)
+library(intergraph)
+library(network)
+library(org.Hs.eg.db)
+
+
+get_topSKAT_degree <- function(enr_df){
+  
+  skat_genes_top100 <- as.character(enr_df[,3])
+  
+  can_net <- read.delim("~/VDLab_scripts/BioGrid/biogrid_db_all_subnet.sif", header = T, sep = " ", stringsAsFactor = F)
+  #can_net <- read.delim("~/RVAS/shard_sub_tier3/DT_sheet/EXOME_isks_risc/test/Biogrid_latest/Biog_net_hs.sif", header = T, sep = "\t", stringsAsFactor = F)
+  can_net_graph <- igraph::graph.data.frame(can_net, directed = F)
+  can_net_graph1 <- igraph::simplify(can_net_graph, remove.loops=T, remove.multiple = T)
+  can_net1 <- igraph::as_data_frame(can_net_graph1, what = "edges")
+  prot_np_all <- unique(can_net1[as.character(can_net1$from) %in% skat_genes_top100 
+                                 & as.character(can_net1$to) %in% skat_genes_top100, ])
+  uniongraph <- igraph::graph.data.frame(prot_np_all, directed = F)
+  
+  
+  
+  ret_df <- as.data.frame(igraph::degree(uniongraph))
+  ret_df$gene <- rownames(ret_df)
+  colnames(ret_df)[1] <- c("degree")
+  
+  l1 <- lapply(ret_df[,2], function(x)as_ids(adjacent_vertices(uniongraph, v = x)[[1]]))
+  l2 <- lapply(l1, function(x)paste(x, sep=",", collapse=","))
+  ret_df$interactors <- unlist(l2)
+  
+  return(ret_df)
+}
 
 df_degree <- get_topSKAT_degree(gender_PC123_cont_df)
 gender_PC123_cont_df$degree <- df_degree[match(gender_PC123_cont_df$gene, df_degree$gene), 1]
@@ -101,9 +138,9 @@ emp_pval_gene <- function(gene_sym){
   xbar <- mean(unlist(gene_add_degree))
   t <- (xbar-a)/(s/sqrt(n))
   ##one sided
-  t_out <- t.test(unlist(gene_add_degree),mu=5,alternative="less")
+  t_out <- t.test(unlist(gene_add_degree),mu=a,alternative="less")
   return(t_out$p.value)
-}
+  }
 s1 <- emp_pval_gene("TP53") #works
 # plot(hist(unlist(s1)))
 # plot(ecdf(unlist(s1)))
@@ -111,11 +148,11 @@ s1 <- emp_pval_gene("TP53") #works
 
 ##use t-distribution for skewed distribution
 # 
-# a <- gender_PC123_cont_df[gender_PC123_cont_df$gene %in% "POT1",]$degree
-#  s <- sd(unlist(s1))
-#  n <- 85
-# xbar <- mean(unlist(s1))
-# t <- (xbar-a)/(s/sqrt(n))
+a <- gender_PC123_cont_df[gender_PC123_cont_df$gene %in% "POT1",]$degree
+ s <- sd(unlist(s1))
+ n <- 85
+xbar <- mean(unlist(s1))
+t <- (xbar-a)/(s/sqrt(n))
 # 2*pt(-abs(t),df=n-1)
 # ##one sided
 # t.test(unlist(s1),mu=5,alternative="less")
