559ad1b05d23bfed1d0d95cee1df2f0d10f18ecc
shu2010 <s.pattnaik@garvan.org.au>
2019-12-23 07:01:51 +0000

    Repset01_script

---
+++ Replication_set/DT_exome_clean_repset.R
@@ -0,0 +1,203 @@
+##Uses scoring function without splice acceptor and donor as C4
+.libPaths(c( "/home/shu/R/x86_64-redhat-linux-gnu-library/3.4", .libPaths() ) )
+
+##Aug6 remove VAF NaN's(multiallelic split issue; error in code for HAIL computation)
+##Aug7 remove NMD variants
+##Rcommendation for SKAT; use a hard filter of comb_score > 5 compared to comb_score >= 5
+##Aug12 changed scoring function, increased penalty for last exon variants (-25)
+##same as DT_call_analysis.R (refer it for source file details)
+##Add filter using VAF > 0.3
+##Use scores for C5 = C4 > C3 = Eigen
+##weights assigned: C5 -> 45; C4 -> 45; C3 -> Eigen; last 5% protein sequence: score/2
+##Scoring function: C5 := Clinvar Pathogenic; C4 := c("frameshift_variant", "start_lost", "stop_gained") 
+
+
+
+Ex_tab <- read.delim("~/RVAS/shard_sub_tier3/DT_sheet/EXOME_isks_risc/test/Rep_set_variants/Rep_set_2044.tsv", sep = "\t",
+                                          header = T, stringsAsFactors = F)
+Ex_tab <- Ex_tab[!is.na(Ex_tab$SAMPLE),]
+Ex_tab$SAMPLE <- gsub(".variant.*$", "", Ex_tab$SAMPLE) ##check this in the next run
+#remove variants ending with Asterisk; these donot have vep annotation
+Ex_tab <- Ex_tab[!is.na(Ex_tab$gene_symbol),]
+
+Ex_tab <- Ex_tab[!is.na(Ex_tab$DP),] ##There are records with NA in DP attribute ; file: sept05_rect
+
+#Ex_tab <- Ex_tab[is.na(Ex_tab$VAF),] ##VAF's have NaNs as DP = 0. Its a division by zero for VAF
+
+#Ex_tab <- Ex_tab[!is.na(Ex_tab$VAF),]
+#Ex_tab <- Ex_tab[Ex_tab$DP != 0,]
+#table(Ex_tab[Ex_tab$VAF == "NaN",]$gene_symbol %in% c("TP53", "TINF2", "POT1", "SMARCAL1"))
+Ex_tab <- Ex_tab[!(Ex_tab$VAF %in% "NaN"),]
+class(Ex_tab$VAF) <- "numeric" ##remove headers from shards in the next run
+Ex_tab <- Ex_tab[!is.na(Ex_tab$VAF),] ##some VAFs are NAs in sept05_rect file
+
+
+DT_gene_tab <- readRDS("~/RVAS/shard_sub_tier3/DT_sheet/DT_gene_tab_all.rds")
+load("~/RVAS/shard_sub_tier3/DT_sheet/EXOME_isks_risc/ASPREE_can.RData")
+
+#DT_gene_tab_ISKS_MGRB_nCH <-DT_gene_tab[DT_gene_tab$cohort %in% c("ISKS", "MGRB no CH"),]
+
+MGRB_noCH <- unique(DT_gene_tab[(DT_gene_tab$cohort) %in% "MGRB no CH",]$SAMPLE)
+
+MGRB_all <- unique(DT_gene_tab[(DT_gene_tab$cohort) %in% c("MGRB no CH", "MGRB"),]$SAMPLE)
+
+Ex_tab$is_CH <- ifelse(Ex_tab$SAMPLE %in% MGRB_noCH, 1, 0)
+
+
+#Ex_tab1 <- Ex_tab[Ex_tab$VAF >= 0.25 & Ex_tab$VAF < 1,] ##OR use the command below to not lose highly significant homozygous variants
+Ex_tab1 <- Ex_tab[as.numeric(Ex_tab$VAF) >= 0.25 & as.numeric(Ex_tab$DP) >= 10,] ##Depth filter
+Ex_tab1$gnomad_AF_NFE <- gsub("\\[", "", Ex_tab1$gnomad_AF_NFE)
+Ex_tab1$gnomad_AF_NFE <- gsub("\\]", "", Ex_tab1$gnomad_AF_NFE)
+class(Ex_tab1$gnomad_AF_NFE) <- "numeric"
+Ex_tab1$gnomad_AF_NFE <- ifelse(is.na(Ex_tab1$gnomad_AF_NFE), 0, Ex_tab1$gnomad_AF_NFE)
+
+#Ex_tab_filt1 <- Ex_tab1[Ex_tab1$gnomad_AF_NFE <= 0.0001, ] ##check this; loses some variants from Aug12 on Sept05 run(don't use)
+
+##remove SAMPLE CR57
+Ex_tab_filt1 <- Ex_tab1[!(Ex_tab1$SAMPLE %in% "CR57"),]
+
+toMatch <- c("3_prime_UTR_variant", "5_prime_UTR_variant", "intron_variant", 
+             "synonymous_variant", "upstream_gene_variant", "non_coding_transcript_exon_variant",
+             "non_coding_transcript_variant", "downstream_gene_variant", "NMD_transcript_variant",
+             "intergenic_variant", "mature_miRNA_variant")
+#"NMD_transcript_variant"
+#Ex_tab_filt2 <- Ex_tab_filt1[!(Ex_tab_filt1$vep_consequence %in% toMatch),]
+Ex_tab_filt2 <- Ex_tab_filt1[!(grepl(paste(toMatch,collapse="|"), 
+                                    Ex_tab_filt1$vep_consequence)),]
+
+
+Ex_tab_filt2$is_MGRB <- ifelse(Ex_tab_filt2$SAMPLE %in% MGRB_all, 1, 0)
+
+Ex_tab_filt2$is_ASPC <- ifelse(Ex_tab_filt2$SAMPLE %in% ASPREE_can$samp, 1, 0)
+
+##filter out MGRB samples that have CH
+
+Ex_tab_noCH_filt3 <- Ex_tab_filt2[!(Ex_tab_filt2$is_CH == 0 & Ex_tab_filt2$is_MGRB == 1),]
+Ex_tab_noCH_filt3 <- Ex_tab_noCH_filt3[Ex_tab_noCH_filt3$is_ASPC == 0,]
+
+
+##Capture variant present in the last 5% of the length of the protein sequence
+
+Ex_tab_noCH_filt3$vep_Protein_position <- ifelse(is.na(Ex_tab_noCH_filt3$vep_Protein_position), 0, Ex_tab_noCH_filt3$vep_Protein_position)
+class(Ex_tab_noCH_filt3$vep_Protein_position) <- "numeric"
+Ex_tab_noCH_filt3$vep_Protein_position <- ifelse(is.na(Ex_tab_noCH_filt3$vep_Protein_position), 0, Ex_tab_noCH_filt3$vep_Protein_position)
+
+#vep_hgvsp <- as.numeric(unlist(regmatches(vep_hgvsp, gregexpr("[[:digit:]]+", vep_hgvsp))))
+#vep_hgvsp <- as.numeric(gsub("[^\\d]", "", vep_hgvsp, perl=TRUE))
+#vep_hgvsp1 <- regmatches(vep_hgvsp, gregexpr("[[:digit:]]+", vep_hgvsp))
+#vep_hgvsp2 <- as.numeric(unlist(lapply(vep_hgvsp1, function(x)x[1])))
+#vep_comb_pos <- ifelse(vep_hgvsp2 == 0, Ex_tab_noCH_filt3$vep_Protein_position, vep_hgvsp2) #works but takes long
+
+vep_hgvsp <- ifelse(is.na(Ex_tab_noCH_filt3$vep_hgvsp), 0, Ex_tab_noCH_filt3$vep_hgvsp)
+vep_hgvsp <- gsub("^*.*p\\.","", vep_hgvsp)
+vep_hgvsp <- gsub("Ter*.*$","", vep_hgvsp)
+library(tidyr)
+vep_hgvsp <- extract_numeric(vep_hgvsp)
+#combine position from vep_Protein_position and vep_hgvsp
+vep_comb_pos <- ifelse(vep_hgvsp == 0, Ex_tab_noCH_filt3$vep_Protein_position, vep_hgvsp)
+Ex_tab_noCH_filt3$vep_comb_pos <- vep_comb_pos
+
+dim(Ex_tab_noCH_filt3[Ex_tab_noCH_filt3$vep_consequence %in% "frameshift_variant",])
+#check redundant records
+#dim(Ex_tab_noCH_filt3[Ex_tab_noCH_filt3$vep_consequence %in% "frameshift_variant" & Ex_tab_noCH_filt3$vep_comb_pos == 0,])
+#Ex_tab_noCH_filt3 <- Ex_tab_noCH_filt3[!is.na(Ex_tab_noCH_filt3$SAMPLE),]
+Ex_tab_noCH_filt3$vep_comb_pos <- ifelse(is.na(Ex_tab_noCH_filt3$vep_comb_pos), 0, Ex_tab_noCH_filt3$vep_comb_pos)
+##remove RNA genes
+Ex_tab_noCH_filt3 <- Ex_tab_noCH_filt3[which(!is.na(as.numeric(Ex_tab_noCH_filt3$LONGEST_GENE_LENGTH_IN_AMINO_ACIDS))),]
+
+Ex_tab_noCH_filt3 <- Ex_tab_noCH_filt3[Ex_tab_noCH_filt3$LONGEST_GENE_LENGTH_IN_AMINO_ACIDS != 0 
+                                       & Ex_tab_noCH_filt3$CANONICAL_GENE_LENGTH_IN_AMIMO_ACIDS != 0,]
+Ex_tab_noCH_filt3$last_five_perc <- ifelse((as.numeric(Ex_tab_noCH_filt3$LONGEST_GENE_LENGTH_IN_AMINO_ACIDS) - Ex_tab_noCH_filt3$vep_comb_pos)/
+                                              (as.numeric(Ex_tab_noCH_filt3$LONGEST_GENE_LENGTH_IN_AMINO_ACIDS)) <= 0.05, 1,
+                                           0)
+
+##David algorithm JUl-22-2019
+toMatch1 <- c("Likely_pathogenic", "Pathogenic", "Pathogenic/Likely_pathogenic", 
+              "Pathogenic/Likely_pathogenic/drug_response", "Pathogenic/Likely_pathogenic/other",
+              "Pathogenic/Affects", "Pathogenic/risk_factor", "Pathogenic/Likely_pathogenic/risk_factor")
+
+
+##modify scoring function since the annotation from the new VEP are slightly different
+toMatch2 <- c("frameshift_variant", "start_lost", "stop_gained")
+
+##sco_fun5 changed on Aug-11-2019 to further underweight last exon variants (aftermath of POT1 variants in MGRB)
+##sco_fun5 changed on Sept-05-2019 to further underweight variants that are C4 and present in the last
+## 5% of the C-terminal tail of the protein.(aftermath of POT1 variants in MGRB)
+sco_fun5 <- function(filt3_inp){
+##construct C5,C4, C3 and score them as sco_fun4  
+  ##All clinvar_sig variants
+  filt3_inp$vep_con1 <- unlist(lapply(strsplit(filt3_inp$vep_consequence, split = "&"), function(x)x[1]))
+  metric1 <-  ifelse(grepl(paste(toMatch1,collapse="|"), 
+                           filt3_inp$clinvar_Clinical_Significance) , "C5", 
+                     ifelse(grepl(paste(toMatch2,collapse="|"), 
+                                  filt3_inp$vep_con1), "C4", 
+                            ifelse(filt3_inp$vep_con1 %in% 
+                                     c("missense_variant", "splice_region_variant",
+                                       "splice_donor_variant", "splice_acceptor_variant",
+                                       "protein_altering_variant")
+                                   & filt3_inp$gnomad_AF_NFE == 0, "C3", "B")))
+   ##lapply(strsplit(var_type, split = "&"), function(x)x[1]) think about this
+
+  metric2 <- as.numeric(ifelse(metric1 %in% "C5", 45, 
+                    ifelse(metric1 %in% "C4", 45, 
+                           ifelse(metric1 %in% "C3", filt3_inp$EigenPhred, 0))))
+  
+  metric2[is.na(metric2)] <- 0
+  
+  ##changed on Aug-11-2019: Adjust this outside the function later
+  #metric3 <- as.numeric(ifelse(metric1 %in% "C4" & filt3_inp$last_exon == 1, -10, 0))
+  #metric3 <- as.numeric(ifelse(metric1 %in% "C4" & filt3_inp$last_exon == 1, -25, 0))
+  
+  #met <- metric2 + metric3
+  
+  #met_df <- cbind.data.frame("auto_call" = metric1, "comb_score" = met )
+  met_df <- cbind.data.frame("auto_call" = metric1, "comb_score" = metric2 )
+  return(met_df)
+}
+
+
+
+##Experiments
+
+##scoring function5
+#DT_gene_tab_ISKS_MGRB_nCH_fil3 <- filt3_inp
+Ex_tab_noCH_filt3 <- cbind.data.frame(Ex_tab_noCH_filt3, sco_fun5(Ex_tab_noCH_filt3))
+
+##Changed on Aug12 after DT meeting
+##for sept05_rect dataset
+##DT nee suggestions: Oct-2-2019
+##If C4 == ClinVar Benign; assign eigenphred score
+toMatch_ben <- c("Benign", "Benign/Likely_benign")
+Ex_tab_noCH_filt3$comb_score <- as.numeric(ifelse(grepl(paste(toMatch_ben,collapse="|"), 
+                                                 Ex_tab_noCH_filt3$clinvar_Clinical_Significance) & 
+                                             Ex_tab_noCH_filt3$auto_call %in% "C4", Ex_tab_noCH_filt3$EigenPhred,
+                                           Ex_tab_noCH_filt3$comb_score))
+#frameshift donot have eigenphred scores, hence they are assigned a score of 15 manually
+Ex_tab_noCH_filt3$comb_score <- ifelse(is.na(Ex_tab_noCH_filt3$comb_score), 15, Ex_tab_noCH_filt3$comb_score)
+##last 5 percent of protein sequence based score penalty
+Cterm_C3_C4 <- c("C3", "C4")
+Ex_tab_noCH_filt3$comb_score <- as.numeric(ifelse(Ex_tab_noCH_filt3$auto_call %in% Cterm_C3_C4 &
+                                                    Ex_tab_noCH_filt3$last_five_perc == 1, (Ex_tab_noCH_filt3$comb_score)/2,
+                                                  Ex_tab_noCH_filt3$comb_score))
+
+##VAF filter: if DP >= 35 use VAF >= 0.3 else VAF > 0.25 (done earlier)
+#if(Ex_tab_noCH_filt3$DP >= 35){
+Ex_tab_noCH_filt4 <- Ex_tab_noCH_filt3[Ex_tab_noCH_filt3$VAF >= 0.3 & Ex_tab_noCH_filt3$DP >= 35, ]
+Ex_tab_noCH_filt5 <- Ex_tab_noCH_filt3[Ex_tab_noCH_filt3$DP < 35, ]
+
+Ex_tab_noCH_filt6 <- rbind.data.frame(Ex_tab_noCH_filt4, Ex_tab_noCH_filt5)
+#saveRDS(Ex_tab_noCH_filt3, file = "~/RVAS/shard_sub_tier3/DT_sheet/EXOME_isks_risc/Exome_filt3_nCH_C5eqC4_noNAs_auto.rds", compress = T)
+#saveRDS(Ex_tab_noCH_filt3, file = "~/RVAS/shard_sub_tier3/DT_sheet/EXOME_isks_risc/Exome_filt3_nCH_C5eqC4_noNAs_auto_4Aug.rds", compress = T)
+#saveRDS(Ex_tab_noCH_filt3, file = "~/RVAS/shard_sub_tier3/DT_sheet/EXOME_isks_risc/Exome_filt3_nCH_C5eqC4_noNAs_auto_6Aug.rds", compress = T)
+#saveRDS(Ex_tab_noCH_filt3, file = "~/RVAS/shard_sub_tier3/DT_sheet/EXOME_isks_risc/Exome_filt3_nCH_C5eqC4_nonmds_auto_7Aug.rds", compress = T)
+#saveRDS(Ex_tab_noCH_filt3, file = "~/RVAS/shard_sub_tier3/DT_sheet/EXOME_isks_risc/Exome_filt3_nCH_C5eqC4_nonmds_iskrisc_12Aug.rds", compress = T)
+#saveRDS(Ex_tab_noCH_filt3, file = "~/RVAS/shard_sub_tier3/DT_sheet/EXOME_isks_risc/Exome_filt3_nCH_C5eqC4_nonmds_iskrisc_05Sept_rect.rds", compress = T)
+#saveRDS(Ex_tab_noCH_filt3, file = "~/RVAS/shard_sub_tier3/DT_sheet/EXOME_isks_risc/Exome_filt3_nCH_C5eqC4_nonmds_iskrisc_05Sept_rect_ASP.rds", compress = T)
+saveRDS(Ex_tab_noCH_filt6, file = "~/RVAS/shard_sub_tier3/DT_sheet/EXOME_isks_risc/test/Rep_set_variants/Rep_set_2044_C5eqC4_nonmds_iskrisclions_Oct08_rect_ASP_new_score.rds", compress = T)
+
+
+#########QC
+#var_check <- read.delim("~/RVAS/shard_sub_tier3/DT_sheet/EXOME_isks_risc/var_control_set_Aug12.tsv", sep = "\t",
+#           header = T)
+
+#table(var_check$VARIANT %in% Ex_tab_noCH_filt3$VARIANT)
---
+++ Replication_set/SKAT_filt_var_bin_mod_Exome2_cMAF_repset.R
@@ -0,0 +1,221 @@
+##Note that this analysis include singletons
+##changes included on Aug-06-2019:
+#added cohort_MAF computation feature; 
+#input is better filtered compared to Aug4 input
+##Aug12: SKAT made feasible for combined ISKS and RISC samples
+#the rare variants are filtered using intra cohort_MAF filter that removes variants that are found in > 3 samples
+#in either case of control(added after sep05 output)
+
+.libPaths(c( "/home/shu/R/x86_64-redhat-linux-gnu-library/3.4", .libPaths() ) )
+
+
+##Using D data structure
+
+library(data.table)
+library(VariantAnnotation)
+library(stringr)
+library(SKAT)
+
+###get input files and process
+##Experiments
+
+fil_tab <- readRDS("~/RVAS/shard_sub_tier3/DT_sheet/EXOME_isks_risc/test/Rep_set_variants/Rep_set_2044_C5eqC4_nonmds_iskrisclions_Oct08_rect_ASP_new_score.rds")
+fil_tab <- fil_tab[!is.na(fil_tab$SAMPLE),]
+dim(fil_tab)
+
+##Additive model
+Ex_samp_id <- unique(fil_tab$SAMPLE)
+Ex_var_id <- unique(fil_tab$VARIANT)
+
+######get phenotype data to control for age and sex and PC's; 
+##note p_Data_PC_comb does not change
+#p_Data <- read.table("~/RVAS/ISKS_MGRB_gender_age_3665.tsv", header = T, sep = "\t")
+p_Data <- read.table("~/RVAS/shard_sub_tier3/DT_sheet/EXOME_isks_risc/test/Rep_set_variants/MGRB_ISKS_1000G_pca_rnd2.scores.tsv", header = T, sep = "\t")
+
+#p_Data$sample <- gsub("isks", "", p_Data$sample)
+#p_Data$sample <- gsub("risc", "", p_Data$sample)
+p_Data_noCH <- p_Data[as.character(p_Data$sample) %in% Ex_samp_id,]
+##drop MGRB sample BAAUD; not in latest call
+#samp_ID_match <- samp_ID_match[grep("BAAUD", samp_ID_match, invert = T)]
+#p_Data_noCH <- p_Data_noCH[match(samp_ID_match, p_Data_noCH$sample),]
+Ex_samp_id <- Ex_samp_id[match(p_Data_noCH$sample, Ex_samp_id)]
+##remove from gene matrix
+#col_rm <- which(colnames(D_tab) ==  "BAAUD")
+#D_tab <- D_tab[,-c(col_rm)]
+##Add gender information
+p_Data2 <- read.table("~/RVAS/shard_sub_tier3/DT_sheet/EXOME_isks_risc/test/Rep_set_variants/joint_calls_2019jan_2019nov.final_qc_output.tsv", header = T, sep = "\t", stringsAsFactors = F)
+
+p_Data <- p_Data[p_Data$sample %in% p_Data2$new_sampleid,]
+p_Data2 <- p_Data2[p_Data2$new_sampleid %in% p_Data$sample,]
+p_Data2 <- p_Data2[match(p_Data2$new_sampleid,p_Data$sample),]
+p_Data$isFemale <- ifelse(p_Data2$f_stat < 0.2, 1, 
+                          ifelse(p_Data2$f_stat > 0.8, 0, 2))
+
+p_Data3 <- read.table("~/RVAS/shard_sub_tier3/DT_sheet/EXOME_isks_risc/test/Rep_set_variants/MGRB3_chip1.all_categories_nb2_depth_and_vaf_and_position_tallies.tsv", header = T, sep = "\t", stringsAsFactors = F)
+p_Data3 <- p_Data3[p_Data3$sample_id %in% p_Data$sample,]
+p_Data$isCHIP <- p_Data3[match(p_Data$sample, p_Data3$sample_id), 4]
+p_Data$isCHIP <- ifelse(grepl("^CN", p_Data$isCHIP), 1, 0)
+##remove MGRB CHIP
+p_Data_noCH <- p_Data[p_Data$isCHIP == 0,]
+
+#binary phenotype vector 
+p_vec <- ifelse(!is.na(as.numeric(as.character(p_Data_noCH$sample))) | grepl("^CR|^LK",as.character(p_Data_noCH$sample)), 1, 0)
+
+##570 cases, 1276 control
+##remove CHIP from variant data
+fil_tab <- fil_tab[fil_tab$SAMPLE %in% p_Data_noCH$sample,]
+
+
+##SKAT null function with customised covariate
+SKAT_fun_null <- function(x=NULL, p_vec){
+  if(is.null(x))
+  obj_N <- SKAT_Null_Model(p_vec ~ 1,out_type="D")
+ # else if(is.integer(x) || is.numeric()){
+  else if(is.null(dim(x))){
+    obj_N <- SKAT_Null_Model(p_vec ~ x,out_type="D")
+  }
+  else if(dim(x)[2] > 1){
+  nul_for <- as.formula(paste("p_vec", paste(colnames(x), collapse = " + "), sep = " ~ "))
+    obj_N <- SKAT_Null_Model(nul_for, data = p_Data_noCH, out_type="D")
+  }
+  return(obj_N)
+  }
+##SKAT function for SKAT, SKATBinary, SKATO, SKAT_ERA
+SKAT_run <- function(geno_mat, gene, x=NULL, p_vec, cust_weight = NULL, rho = NULL){
+  
+  if(dim(geno_mat)[2] > 1 ){
+    null_d <- SKAT_fun_null(x, p_vec)
+    pval_SKAT <- SKAT(t(geno_mat), null_d, weights = cust_weight, r.corr = rho)$p.value
+    pval_SKATbin <- SKATBinary(t(geno_mat), null_d, method = "Burden", weights = cust_weight)$p.value
+    ##ER: Effective resampling for best p-value calculation
+   # pval_SKATbin <- SKATBinary(t(geno_mat), null_d, method.bin = "ER", weights = cust_weight, r.corr = rho)$p.value
+  #  pval_SKATO <- SKAT(t(geno_mat),null_d, method='optimal.adj', weights = cust_weight)$p.value ##r.corr will be ignored
+    pval_SKATO <- SKATBinary(t(geno_mat),null_d, method='SKATO', weights = cust_weight)$p.value
+    pval_SKATera <- SKATBinary(t(geno_mat),null_d, method.bin="ER.A", weights = cust_weight)$p.value
+    skat_pv <- cbind.data.frame("eg_ID" = gene, pval_SKAT, pval_SKATbin, pval_SKATO, pval_SKATera)
+    return(skat_pv)
+  }
+  else if(dim(geno_mat)[2] == 1){
+    null_d <- SKAT_fun_null(x, p_vec)
+    pval_SKAT <- SKAT(geno_mat, null_d, weights = cust_weight, r.corr = rho)$p.value
+    pval_SKATbin <- SKATBinary(t(geno_mat), null_d, method = "Burden", weights = cust_weight)$p.value
+    ##ER: Effective resampling for best p-value calculation
+    # pval_SKATbin <- SKATBinary(t(geno_mat), null_d, method.bin = "ER", weights = cust_weight, r.corr = rho)$p.value
+    #pval_SKATO <- SKAT(geno_mat,null_d, method='optimal.adj', weights = cust_weight)$p.value ##r.corr will be ignored
+    pval_SKATO <- SKATBinary(geno_mat,null_d, method='SKATO', weights = cust_weight)$p.value
+    pval_SKATera <- SKATBinary(geno_mat,null_d, method.bin="ER.A", weights = cust_weight)$p.value
+    skat_pv <- cbind.data.frame("eg_ID" = gene, pval_SKAT, pval_SKATbin, pval_SKATO, pval_SKATera)
+    
+    return(skat_pv)
+  }
+}
+
+##Run SKAT on variants collapsed by gene
+
+DT_skat_snv_str_pc123 <- list()
+genes <- unique(fil_tab$gene_symbol)
+
+    skat_pvals <- list()
+    skat_pvals_pc12 <- list()
+    skat_pvals_sex <- list()
+    skat_pvals_sex_pc12 <- list()
+#    skat_pvals_age <- list()
+ #   skat_pvals_age_pc12 <- list()
+ #   skat_pvals_age_sex <- list()
+#    skat_pvals_age_sex_pc12 <- list()
+    
+    for(i in 1:length(genes)){
+      print(i)
+      print(genes[i])
+      ##process genes in a loop
+      ftemp_tab <- fil_tab[fil_tab$gene_symbol %in% genes[i],]
+      ftemp_tab <- ftemp_tab[ftemp_tab$comb_score >= 5 & as.numeric(ftemp_tab$VAF) >= 0.25, ]
+      
+      print(max(ftemp_tab$comb_score))
+      if(dim(ftemp_tab) == 0 ){
+        next
+      }
+      else{
+        ftemp_tab_var_id <- unique(ftemp_tab$VARIANT)
+        samp_vec <- list()
+        for(m in 1:length(ftemp_tab_var_id)){
+          sam_gene_gt <- ftemp_tab[ftemp_tab$VARIANT %in% ftemp_tab_var_id[m],][,c(1:3,9,11,130:131)]
+          sam_gene_gt <- unique(sam_gene_gt)
+          ##compute cohort specific MAF
+           maf_vec_cont <- sum(ifelse(is.na(as.numeric(sam_gene_gt$SAMPLE)), 1, 0))/(2*1270)
+           maf_vec_case <- sum(ifelse(!is.na(as.numeric(sam_gene_gt$SAMPLE)) | 
+                                    grepl("^CR", as.character(sam_gene_gt$SAMPLE)), 1, 0))/(2*570)
+           #maf_vec <- (maf_vec_cont + maf_vec_case)/(2*(1572 + 1110))
+          ##genotype matrix  
+          sam_gene_gt$add_mod <- as.numeric(sam_gene_gt$GT)
+          sam10 <- ifelse(Ex_samp_id %in% sam_gene_gt$SAMPLE, 1, 0)
+          sam10[which(sam10 != 0)] <- sam_gene_gt$add_mod ##additive model
+          
+          samp_vec[[m]] <- c(ftemp_tab_var_id[m],
+                             unique(sam_gene_gt$gene_symbol), 
+                             unique(sam_gene_gt$vep_consequence), 
+                             unique(as.character(sam_gene_gt$auto_call)),
+                             as.numeric(unique(sam_gene_gt$comb_score)), as.numeric(maf_vec_case),
+                             as.numeric(maf_vec_cont), sam10)
+        }
+       
+        samp_vec_mat <- do.call("rbind.data.frame", samp_vec)
+      print(dim(samp_vec_mat))
+      colnames(samp_vec_mat) <- c("VARIANT", "gene_symbol", 
+                                  "vep_consequence", "auto_call", "comb_score", "coh_MAF_case",
+                                  "coh_MAF_cont", Ex_samp_id)
+     ##Intracohort filter  : equivalent to ~ 3/1110*2 for case ; ~ 3/1562*2
+  #     samp_vec_mat <- samp_vec_mat[as.numeric(as.character(samp_vec_mat$coh_MAF_case)) <= 0.0015 | 
+  #                                    as.numeric(as.character(samp_vec_mat$coh_MAF_cont)) <= 0.001,]
+       samp_vec_mat <- samp_vec_mat[!(as.numeric(as.character(samp_vec_mat$coh_MAF_case)) >= 0.0015 | 
+                                      as.numeric(as.character(samp_vec_mat$coh_MAF_cont)) >= 0.001),]
+    #  samp_vec_mat <- samp_vec_mat[as.numeric(as.character(samp_vec_mat$coh_MAF_cont)) <= 0.001,]
+      
+      if(is.null(dim(samp_vec_mat)) | dim(samp_vec_mat)[1] == 0) {
+        skat_pvals[[i]] <- NULL
+        skat_pvals_pc12[[i]] <- NULL
+        skat_pvals_sex[[i]] <- NULL
+        skat_pvals_sex_pc12[[i]] <- NULL
+       # skat_pvals_age[[i]] <- NULL
+       # skat_pvals_age_pc12[[i]] <- NULL
+       # skat_pvals_age_sex[[i]] <- NULL
+       # skat_pvals_age_sex_pc12[[i]] <- NULL
+        #   break
+        next
+      }
+      ##compute maf SKAT takes care of the AFs internally by assigning higher weights to rare variants 
+      
+      else {
+        gene_mat_comb <- as.numeric(as.character(samp_vec_mat[,5]))
+        gene_mat <- as.matrix(samp_vec_mat[,-c(1:7)])
+        class(gene_mat) <- "numeric"
+        ##for strictly burden test set rho = 1, else set rho = 0
+        ##no covariate
+        skat_pvals[[i]] <- SKAT_run(geno_mat = gene_mat, gene = genes[i], x=NULL, p_vec, cust_weight = gene_mat_comb, rho = 1)
+        ##only pc1 + pc2
+        skat_pvals_pc12[[i]] <- SKAT_run(geno_mat = gene_mat, gene = genes[i], x=p_Data_noCH[,c(19:21)], p_vec, cust_weight = gene_mat_comb, rho = 1)
+        ##gender
+        skat_pvals_sex[[i]] <- SKAT_run(geno_mat = gene_mat, gene = genes[i], x=p_Data_noCH$isFemale, p_vec, cust_weight = gene_mat_comb, rho = 1)
+        ##gender + pc1 + pc2
+        skat_pvals_sex_pc12[[i]] <- SKAT_run(geno_mat = gene_mat, gene = genes[i], x=p_Data_noCH[,c(13,19:21)], p_vec, cust_weight = gene_mat_comb, rho = 1)
+        
+      }
+  
+    } ##end of nested else loop
+    
+      }##end of gene loop ; i loop   
+  
+    
+    DT_skat_snv_str_pc123 <- list(skat_pvals, skat_pvals_pc12, skat_pvals_sex, skat_pvals_sex_pc12)
+  
+    #saveRDS(DT_skat_snv_str_pc123, file = "~/RVAS/shard_sub_tier3/DT_sheet/EXOME_isks_risc/Exome_skat_wsing_load123_noCH_C5eqC4_noNA_gt.rds", compress = T)   
+   # saveRDS(DT_skat_snv_str_pc123, file = "~/RVAS/shard_sub_tier3/DT_sheet/EXOME_isks_risc/Exome_skat_wsing_load123_noCH_C5eqC4_noNA_gt_4Aug.rds", compress = T)    
+   # saveRDS(DT_skat_snv_str_pc123, file = "~/RVAS/shard_sub_tier3/DT_sheet/EXOME_isks_risc/Exome_skat_wsing_load123_noCH_C5eqC4_noNA_gt_6Aug.rds", compress = T)
+   # saveRDS(DT_skat_snv_str_pc123, file = "~/RVAS/shard_sub_tier3/DT_sheet/EXOME_isks_risc/Exome_skat_wsing_load123_noCH_C5eqC4_nonmds_gt_7Aug.rds", compress = T)   
+   # saveRDS(DT_skat_snv_str_pc123, file = "~/RVAS/shard_sub_tier3/DT_sheet/EXOME_isks_risc/Exome_skat_wsing_load123_noCH_C5eqC4_nonmds_gt_isksrisc_12Aug_rerun.rds", compress = T)
+   ##SKAT ERA added along with changes to SKATO; now SKATBinary is used for SKATO and SKAT_ERA
+    #ERA -> Adaptive resampling for conservative p-values
+   # saveRDS(DT_skat_snv_str_pc123, file = "~/RVAS/shard_sub_tier3/DT_sheet/EXOME_isks_risc/Exome_skat_wsing_load123_noCH_C5eqC4_nonmds_gt_isksrisc_12Aug_rerun.rds", compress = T)
+   # saveRDS(DT_skat_snv_str_pc123, file = "~/RVAS/shard_sub_tier3/DT_sheet/EXOME_isks_risc/Exome_skat_wsing_load123_noCH_C5eqC4_nonmds_gt_isksrisc_sept05_rect.rds", compress = T) 
+    saveRDS(DT_skat_snv_str_pc123, file = "~/RVAS/shard_sub_tier3/DT_sheet/EXOME_isks_risc/test/Rep_set_variants/Exome_skat_wsing_load123_noCH_C5eqC4_repset_nov2019.rds", compress = T)
+    
